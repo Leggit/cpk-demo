732ea9b1a81bab20c33f6dae863b412fae8d8aab
oliver Legg <leggo646@gmail.com>
2025-07-05 12:54:43 +0100

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,233 @@
-import Image from "next/image";
+"use client";
+
+import { Calendar } from "@/components/ui/calendar";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { useCopilotAction } from "@copilotkit/react-core";
+import { useState } from "react";
+
+import dynamic from "next/dynamic";
+
+const CopilotSidebar = dynamic(
+  () => import("@copilotkit/react-ui").then((mod) => mod.CopilotSidebar),
+  { ssr: false } // <-- The key part!
+);
+
+type Form = {
+  title: string;
+  elements: {
+    type: "text" | "date" | "radio";
+    config: { [key: string]: any };
+  }[];
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [form, setForm] = useState<Form>({
+    title: "",
+    elements: [],
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useCopilotAction({
+    name: "setFormTitle",
+    description: "Set the title of the form that is being built",
+    parameters: [
+      {
+        name: "title",
+        type: "string",
+        description: "The title of the form",
+        required: true,
+      },
+    ],
+    handler: async ({ title }) => {
+      setForm((prevForm) => ({
+        ...prevForm,
+        title,
+      }));
+    },
+  });
+
+  useCopilotAction({
+    name: "addTextInputElement",
+    description: "Add a text input to the form",
+    parameters: [
+      {
+        name: "label",
+        type: "string",
+        description: "The label for the text input",
+        required: true,
+      },
+      {
+        name: "required",
+        type: "boolean",
+        description: "Whether the text input is required",
+        required: false,
+      },
+    ],
+    handler: async ({ label, required }) => {
+      setForm((prevForm) => ({
+        ...prevForm,
+        elements: [
+          ...prevForm.elements,
+          { type: "text", config: { required, label } },
+        ],
+      }));
+    },
+  });
+
+  useCopilotAction({
+    name: "addRadioInputElement",
+    description: "Add a radio input to the form",
+    parameters: [
+      {
+        name: "label",
+        type: "string",
+        description: "The label for the radio input",
+        required: true,
+      },
+      {
+        name: "options",
+        description:
+          "Options for the radio input, each option should be a string",
+        type: "object[]",
+        attributes: [
+          {
+            name: "value",
+            type: "string",
+            description: "The value of the radio option",
+            required: true,
+          },
+          {
+            name: "label",
+            type: "string",
+            description: "The label for the radio option",
+            required: true,
+          },
+        ],
+        required: true,
+      },
+      {
+        name: "required",
+        type: "boolean",
+        description: "Whether the radio input is required",
+        required: false,
+      },
+    ],
+    handler: async ({ label, options, required }) => {
+      setForm((prevForm) => ({
+        ...prevForm,
+        elements: [
+          ...prevForm.elements,
+          { type: "radio", config: { label, options, required } },
+        ],
+      }));
+    },
+  });
+
+  useCopilotAction({
+    name: "addDateInputElement",
+    description: "Add a date input to the form",
+    parameters: [
+      {
+        name: "label",
+        type: "string",
+        description: "The label for the date input",
+        required: true,
+      },
+      {
+        name: "required",
+        type: "boolean",
+        description: "Whether the date input is required",
+        required: false,
+      },
+      {
+        name: "defaultMonth",
+        type: "string",
+        description:
+          "The default month for the date input in ISO format (YYYY-MM-DD)",
+        required: false,
+      },
+    ],
+    handler: async ({ label, required }) => {
+      setForm((prevForm) => ({
+        ...prevForm,
+        elements: [
+          ...prevForm.elements,
+          { type: "date", config: { label, required } },
+        ],
+      }));
+    },
+  });
+
+  return (
+    <>
+      <div className="max-w-3xl p-4 mx-auto">
+        <h1>{form.title}</h1>
+        {form.elements.map((element, index) => {
+          switch (element.type) {
+            case "text":
+              return (
+                <div key={index} className="p-4 m-2 shadow-md rounded-md">
+                  <Label className="mb-2">
+                    {element.config.label}
+                    {element.config.required && " *"}
+                  </Label>
+                  <Input required={element.config.required} type="text" />
+                </div>
+              );
+            case "radio":
+              return (
+                <div key={index} className="p-4 m-2 shadow-md rounded-md">
+                  <div className="mb-2">
+                    {element.config.label}
+                    {element.config.required && " *"}
+                  </div>
+                  <RadioGroup
+                    defaultValue={element.config.defaultValue}
+                    required={element.config.required}
+                  >
+                    {element.config.options.map((option: any, idx: number) => (
+                      <div key={idx} className="flex items-center gap-3">
+                        <RadioGroupItem
+                          value={option.value}
+                          id={option.value + idx}
+                        />
+                        <Label htmlFor={option.value + idx}>
+                          {option.label}
+                        </Label>
+                      </div>
+                    ))}
+                  </RadioGroup>
+                </div>
+              );
+            case "date":
+              return (
+                <div key={index} className="p-4 m-2 shadow-md rounded-md">
+                  <Label className="mb-2">
+                    {element.config.label}
+                    {element.config.required && " *"}
+                  </Label>
+                  <Calendar
+                    mode="single"
+                    defaultMonth={
+                      element.config.defaultMonth
+                        ? new Date(element.config.defaultMonth)
+                        : undefined
+                    }
+                  />
+                </div>
+              );
+          }
+        })}
+      </div>
+      <CopilotSidebar
+        clickOutsideToClose={false}
+        defaultOpen={true}
+        labels={{
+          title: "Form builder assistant",
+          initial: "Describe your form to me",
+        }}
+      />
+    </>
   );
 }
